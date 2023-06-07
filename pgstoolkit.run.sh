6a9ee3f48b7cfc1ae47654ff32e3aca8e7587454
Sander W. van der Laan <s.w.vanderlaan@gmail.com>
2023-06-07 11:26:26 +0200

    Small fixes to run-script

--- pgstoolkit.run.sh
+++ pgstoolkit.run.sh
@@ -36,29 +36,29 @@ AEGSDIR="${AEGSDATADIR}/AEGS_COMBINED_EAGLE2_1000Gp3v5HRCr11"
 
 echo ""
 echo "Converting VCF to bgen."
-this data includes the correct ID-type (chr:BP, e.g. 1:10711)
-for CHR in $(seq 1 22 ); do
-  
-  echo "> converting chromosome ${CHR}..."
-  echo "...8-bits version and indexing"
-  $PLINK --vcf ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.chr${CHR}.vcf.gz --export bgen-1.2 bits=8 --out ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.8bit.chr${CHR}
-  $BGENIX -index -g ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.8bit.chr${CHR}.bgen 
-  
-  echo ""
-  echo "...16-bits version (default) and indexing"
-  $PLINK --vcf ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.chr${CHR}.vcf.gz --export bgen-1.2 --out ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.16bit.chr${CHR}
-  $BGENIX -index -g ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.16bit.chr${CHR}.bgen
-  
-done
+### This data includes the correct ID-type (chr:BP, e.g. 1:10711)
+# for CHR in $(seq 1 22 ); do
+#   
+#   echo "> converting chromosome ${CHR}..."
+#   echo "...8-bits version and indexing"
+#   $PLINK --vcf ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.chr${CHR}.vcf.gz --export bgen-1.2 bits=8 --out ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.8bit.chr${CHR}
+#   $BGENIX -index -g ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.8bit.chr${CHR}.bgen 
+#   
+#   echo ""
+#   echo "...16-bits version (default) and indexing"
+#   $PLINK --vcf ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.chr${CHR}.vcf.gz --export bgen-1.2 --out ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.16bit.chr${CHR}
+#   $BGENIX -index -g ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.16bit.chr${CHR}.bgen
+#   
+# done
 
 echo ""
 echo "Creating variant lists."
-for CHR in $(seq 1 22); do 
-	bgenix_20230202 \
-	-g ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.chr${CHR}.bgen \
-	-i ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.chr${CHR}.bgen.bgi \
-	-list | grep -v "#" > ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.chr${CHR}.variantlist.txt; 
-done
+# for CHR in $(seq 1 22); do 
+# 	bgenix_20230202 \
+# 	-g ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.chr${CHR}.bgen \
+# 	-i ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.chr${CHR}.bgen.bgi \
+# 	-list | grep -v "#" > ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.chr${CHR}.variantlist.txt; 
+# done
 
 # Variantlist looks like this
 # alternate_ids	rsid	chromosome	position	number_of_alleles	first_allele	alternative_alleles
@@ -70,13 +70,13 @@ done
 echo ""
 echo "Concatenating variant list."
 
-echo "variantid alt_ids rsid_aegs chromosome position number_of_alleles first_allele alt_alleles" > ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.variantlist.txt
-
-for CHR in $(seq 1 22); do 
-	cat ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.chr${CHR}.variantlist.txt | \
-	awk '{ print $3":"$4, $1, $2, $3, $4, $5, $6, $7}' | tail -n +2 >> ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.variantlist.txt 
-done
-gzip -v ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.variantlist.txt
+# echo "variantid alt_ids rsid_aegs chromosome position number_of_alleles first_allele alt_alleles" > ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.variantlist.txt
+# 
+# for CHR in $(seq 1 22); do 
+# 	cat ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.chr${CHR}.variantlist.txt | \
+# 	awk '{ print $3":"$4, $1, $2, $3, $4, $5, $6, $7}' | tail -n +2 >> ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.variantlist.txt 
+# done
+# gzip -v ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.variantlist.txt
 
 # Concatenated variantlist looks like this
 # variantid alt_ids rsid_aegs chromosome position number_of_alleles first_allele alt_alleles
@@ -89,64 +89,63 @@ gzip -v ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.variantlist.txt
 echo ""
 echo "Calculating frequencies."
 
-for CHR in $(seq 1 22); do 
-	echo "> calculating frequencies for chromosome ${CHR}..."
-	$PLINK \
-	--bgen ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.chr${CHR}.bgen 'ref-first' \
-	--sample ${AEGSDATADIR}//AEGS_COMBINED_EAGLE2_1000Gp3v5HRCr11_pheno/20200419.QC.AEGS123.forPGSToolKit.sample \
-	--freq --out ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.chr${CHR}.FREQ; 
-done
+# for CHR in $(seq 1 22); do 
+# 	echo "> calculating frequencies for chromosome ${CHR}..."
+# 	$PLINK \
+# 	--bgen ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.chr${CHR}.bgen 'ref-first' \
+# 	--sample ${AEGSDATADIR}//AEGS_COMBINED_EAGLE2_1000Gp3v5HRCr11_pheno/20200419.QC.AEGS123.forPGSToolKit.sample \
+# 	--freq --out ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.chr${CHR}.FREQ; 
+# done
 
 echo ""
-echo "Creating input for PRSToolKit."
+echo "Creating input for PGSToolKit."
 
-# Head INOUYE
-# chr	position	rsid	allele1	allele2	effect_allele	beta
-#  1	  2245570	rs2843152	C	G	G	-2.76009e-02
-#  1	 22132518	rs35465346	A	G	G	 2.39340e-02
-#  1	 38386727	rs28470722	A	G	G	-1.74935e-02
-
-echo "variantid rsid rsid_aegs chromosome position effect_allele other_allele beta P_ukbb" > ${PGS_CAD}/metaGRS_hg19_20180205.foo
-zcat ${PGS_CAD}/metaGRS_hg19_20180205.txt.gz | \
-parseTable --col chr,position,rsid,allele1,allele2,effect_allele,beta | \
-awk '{ if($6 == $5) { print $1":"$2, $3, $3, $1, $2, $6, $4, $7, "NA" } else { print $1":"$2, $3, $3, $1, $2, $6, $5, $7, "NA" } }' | tail -n +2 >> ${PGS_CAD}/metaGRS_hg19_20180205.foo
-
-mergeTablesv2 \
---file1 ${PGS_CAD_UKBB}/UKBB.GWAS1KG.EXOME.CAD.SOFT.META.PublicRelease.300517.4pvalupdate.txt \
---file2 ${PGS_CAD}/metaGRS_hg19_20180205.foo \
---index variantid --format NORM --replace > ${PGS_CAD}/metaGRS_hg19_20180205.4PGSTK.foo
-
-mergeTablesv2 \
---file1 ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.variantlist.txt.gz \
---file2 ${PGS_CAD}/metaGRS_hg19_20180205.4PGSTK.foo \
---index variantid --format GZIP1 --replace > ${PGS_CAD}/metaGRS_hg19_20180205.4PGSTK.txt
+# # Head INOUYE
+# # chr	position	rsid	allele1	allele2	effect_allele	beta
+# #  1	  2245570	rs2843152	C	G	G	-2.76009e-02
+# #  1	 22132518	rs35465346	A	G	G	 2.39340e-02
+# #  1	 38386727	rs28470722	A	G	G	-1.74935e-02
+# 
+# echo "variantid rsid rsid_aegs chromosome position effect_allele other_allele beta P_ukbb" > ${PGS_CAD}/metaGRS_hg19_20180205.foo
+# zcat ${PGS_CAD}/metaGRS_hg19_20180205.txt.gz | \
+# parseTable --col chr,position,rsid,allele1,allele2,effect_allele,beta | \
+# awk '{ if($6 == $5) { print $1":"$2, $3, $3, $1, $2, $6, $4, $7, "NA" } else { print $1":"$2, $3, $3, $1, $2, $6, $5, $7, "NA" } }' | tail -n +2 >> ${PGS_CAD}/metaGRS_hg19_20180205.foo
+# 
+# mergeTablesv2 \
+# --file1 ${PGS_CAD_UKBB}/UKBB.GWAS1KG.EXOME.CAD.SOFT.META.PublicRelease.300517.4pvalupdate.txt \
+# --file2 ${PGS_CAD}/metaGRS_hg19_20180205.foo \
+# --index variantid --format NORM --replace > ${PGS_CAD}/metaGRS_hg19_20180205.4PGSTK.foo
+# 
+# mergeTablesv2 \
+# --file1 ${AEGSDIR}/aegs.qc.1kgp3hrcr11.idfix.rsid.8bit.variantlist.txt.gz \
+# --file2 ${PGS_CAD}/metaGRS_hg19_20180205.4PGSTK.foo \
+# --index variantid --format GZIP1 --replace > ${PGS_CAD}/metaGRS_hg19_20180205.4PGSTK.txt
 
 echo ""
 echo "Running PGSToolKit."
 
 #################################################################################################
 ### PARAMETERS SLURM YOU SHOULD PROVIDE
-### --job-name=pgsTK                                  			# the name of the job
-### --output=/hpc/dhl_ec/svanderlaan/projects/pgsTK.log 	   	# the log file of this job
-### --error=/hpc/dhl_ec/svanderlaan/projects/pgsTK.errors		# the error file of this job
-### --time=12:15:00                                       		# the amount of time the job will take: -t [min] OR -t [days-hh:mm:ss]
-### --mem=48G                                             		# the amount of memory you think the script will consume, found on: https://wiki.bioinformatics.umcutrecht.nl/bin/view/HPC/SlurmScheduler
-### --gres=tmpspace:128G                                  		# the amount of temporary diskspace per node
-### --mail-user=yourname@domain.com            					# where should be mailed to?
-### --mail-type=FAIL                                      		# when do you want to receive a mail from your job?  Valid type values are NONE, BEGIN, END, FAIL, REQUEUE
-                                                              		# or ALL (equivalent to BEGIN, END, FAIL, INVALID_DEPEND, REQUEUE, and STAGE_OUT), 
-                                                              		# Multiple type values may be specified in a comma separated list. 
-####    Note:   You do not have to specify workdir: 
-####            'Current working directory is the calling process working directory unless the --chdir argument is passed, which will override the current working directory.'
-####            TODO: select the type of interpreter you'd like to use
-####            TODO: Find out whether this job should dependant on other scripts (##SBATCH --depend=[state:job_id])
+### --job-name=pgsTK												# the name of the job
+### --output=/hpc/dhl_ec/svanderlaan/projects/pgsTK.log 			# the log file of this job
+### --error=/hpc/dhl_ec/svanderlaan/projects/pgsTK.errors			# the error file of this job
+### --time=12:15:00													# the amount of time the job will take: -t [min] OR -t [days-hh:mm:ss]
+### --mem=48G														# the amount of memory you think the script will consume, found on: https://wiki.bioinformatics.umcutrecht.nl/bin/view/HPC/SlurmScheduler
+### --gres=tmpspace:128G											# the amount of temporary diskspace per node
+### --mail-user=yourname@domain.com									# where should be mailed to?
+### --mail-type=FAIL												# when do you want to receive a mail from your job?  Valid type values are NONE, BEGIN, END, FAIL, REQUEUE
+																	# or ALL (equivalent to BEGIN, END, FAIL, INVALID_DEPEND, REQUEUE, and STAGE_OUT), 
+																	# Multiple type values may be specified in a comma separated list. 
+####	Note:	You do not have to specify workdir: 
+####			'Current working directory is the calling process working directory unless the --chdir argument is passed, which will override the current working directory.'
+####			TODO: select the type of interpreter you'd like to use
+####			TODO: Find out whether this job should dependant on other scripts (##SBATCH --depend=[state:job_id])
 ####
 #################################################################################################
 
-
-### INOUYE
+### INOUYE CAD
 sbatch --job-name=pgsTK_CAD --output=${PROJECTDIR}/PGS/pgsTK_CAD.log --error=${PROJECTDIR}/PGS/pgsTK_CAD.errors  \
 --time=12:15:00 --mem=48G --gres=tmpspace:128G \
 --mail-user=s.w.vanderlaan-2@umcutrecht.nl --mail-type=FAIL \
-${PROJECTDIR}/PGS/pgstoolkit.sh ${PROJECTDIR}/PGS/pgstoolkit.cad.config
+${SOFTWARE}/PGSToolKit/pgstoolkit.sh ${PROJECTDIR}/PGS/pgstoolkit.cad.config
 

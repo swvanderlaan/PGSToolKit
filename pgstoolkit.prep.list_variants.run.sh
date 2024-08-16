bfde069195f10ff09134cbd998e21474e493bd03
Sander W. van der Laan <s.w.vanderlaan@gmail.com>
2024-08-16 11:17:51 -0400

    fix array

--- pgstoolkit.prep.list_variants.run.sh
+++ pgstoolkit.prep.list_variants.run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Array job: list variants
-# CHR=${SLURM_ARRAY_TASK_ID}  # The chromosome number automatically assigned by SLURM
-# BGENIX=$1
-# STUDYDIR=$2
-# Set variables
-CHR=$1  # The chromosome number 
-BGENIX=$2
-STUDYDIR=$3
+CHR=${SLURM_ARRAY_TASK_ID}  # The chromosome number automatically assigned by SLURM
+BGENIX=$1
+STUDYDIR=$2
+# Set variables - for manual per chromosome individual job version
+# CHR=$1  # The chromosome number 
+# BGENIX=$2
+# STUDYDIR=$3
 # List the variants
 $BGENIX - g ${STUDYDIR}/aegscombo.topmed_r3_f10_b38.split_norm_af_filter.8bit.chr${CHR}.bgen \
 -i ${STUDYDIR}/aegscombo.topmed_r3_f10_b38.split_norm_af_filter.8bit.chr${CHR}.bgen.bgi \

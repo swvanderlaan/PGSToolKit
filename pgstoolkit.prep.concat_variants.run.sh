f19edf058c3bfb40f75fc8dea7bc6d71a64f75b8
Sander W. van der Laan <s.w.vanderlaan@gmail.com>
2024-08-16 15:16:22 -0400

    fix

--- pgstoolkit.prep.concat_variants.run.sh
+++ pgstoolkit.prep.concat_variants.run.sh
@@ -7,7 +7,7 @@ for CHR in $(seq 1 22); do
 	cat ${STUDYDIR}/aegscombo.topmed_r3_f10_b38.split_norm_af_filter.8bit.chr${CHR}.variantlist.txt | \
 	awk '{ print $3":"$4, $1, $2, $3, $4, $5, $6, $7}' | tail -n +2 >> ${STUDYDIR}/aegscombo.topmed_r3_f10_b38.split_norm_af_filter.8bit.variantlist.txt 
 done
-gzip -v ${STUDYDIR}/aegscombo.topmed_r3_f10_b38.split_norm_af_filter.8bit.variantlist.txt
+gzip -vf ${STUDYDIR}/aegscombo.topmed_r3_f10_b38.split_norm_af_filter.8bit.variantlist.txt
 
 # 16 bit
 echo "variantid alt_ids rsid_aegs chromosome position number_of_alleles first_allele alt_alleles" > ${STUDYDIR}/aegscombo.topmed_r3_f10_b38.split_norm_af_filter.16bit.variantlist.txt
@@ -15,4 +15,4 @@ for CHR in $(seq 1 22); do
 	cat ${STUDYDIR}/aegscombo.topmed_r3_f10_b38.split_norm_af_filter.16bit.chr${CHR}.variantlist.txt | \
 	awk '{ print $3":"$4, $1, $2, $3, $4, $5, $6, $7}' | tail -n +2 >> ${STUDYDIR}/aegscombo.topmed_r3_f10_b38.split_norm_af_filter.16bit.variantlist.txt 
 done
-gzip -v ${STUDYDIR}/aegscombo.topmed_r3_f10_b38.split_norm_af_filter.16bit.variantlist.txt
+gzip -vf ${STUDYDIR}/aegscombo.topmed_r3_f10_b38.split_norm_af_filter.16bit.variantlist.txt
